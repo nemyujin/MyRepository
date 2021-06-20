6090dfe7d89843db77d6ee555266ad57ff0c2b96
홍유진 <@DESKTOP-0MT66Q0>
2021-06-21 01:19:05 +0900

    first

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -3,6 +3,8 @@ public class MyClass {
  public static void main(String args[]) {
 	 System.out.println("Hello Git!");
 	 System.out.println("This is 2nd version");
+	 MyHello myHello = new MyHello();
+	 myHello.sayHello();
 	
 
  }
---
+++ MyGit Java/src/B.java
@@ -0,0 +1,9 @@
+
+public class B {
+	public static void main(String args[]) {
+	 System.out.println("Hello Git!");
+	 System.out.println("This is B version");
+	 System.out.println("This is B branch");
+	}
+	
+}
