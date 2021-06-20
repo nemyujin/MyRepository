4f38e782610f23a9461f145e5eeb6fafe59ca56b
홍유진 <@DESKTOP-0MT66Q0>
2021-06-21 00:56:52 +0900

    dev branch

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -2,9 +2,8 @@
 public class MyClass {
  public static void main(String args[]) {
 	 System.out.println("Hello Git!");
-	 System.out.println("This is 2nd version");
-	 MyHello myHello = new MyHello();
-	 myHello.sayHello();
+	 System.out.println("sayHello change in master branch");
+
 	
  }
 }
